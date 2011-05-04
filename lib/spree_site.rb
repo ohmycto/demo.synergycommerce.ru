d5bccc2180a31f6b103f8eb0b747e1344cdb4d66
Roman Smirnov <roman@railsdog.com>
2011-05-04 17:35:28 +0400

    Added site extension

---
+++ lib/spree_site.rb
@@ -0,0 +1,14 @@
+module SpreeSite
+  class Engine < Rails::Engine
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.env.production? ? require(c) : load(c)
+      end
+    end
+    
+    def load_tasks
+    end
+    
+    config.to_prepare &method(:activate).to_proc
+  end
+end
