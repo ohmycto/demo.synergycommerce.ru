8b83ef1f53b3bac20c57258c5799f372a1d80ed9
Roman Smirnov <roman@railsdog.com>
2011-05-05 13:42:15 +0400

    Revert "spree_enhanced_option_types:install"
    This reverts commit b415b143dac161c7a2580e6bf1f5e2dbccf0b88b.

--- db/migrate/20100825095803_add_sku_to_option_values.rb
+++
@@ -1,9 +0,0 @@
-class AddSkuToOptionValues < ActiveRecord::Migration
-  def self.up
-    add_column :option_values, :sku, :string
-  end
-
-  def self.down
-    remove_column :option_values, :sku
-  end
-end
