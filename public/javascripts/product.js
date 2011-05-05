f6294949d39e697dae76d744091a061c2fa3ba05
Roman Smirnov <roman@railsdog.com>
2011-05-05 13:49:03 +0400

    Update synergy_default_theme

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -2,12 +2,12 @@ var add_image_handlers = function() {
   $('ul.thumbnails li').eq(0).addClass('selected');
   $('ul.thumbnails li a').click(function(e) {
     var selectedThumb = $(this);
-    $('#main-image img').attr('src', selectedThumb.attr('product'));
+    $('#main-image img').attr('src', selectedThumb.attr('href'));
     $('ul.thumbnails li').removeClass('selected');
     $(this).parent('li').addClass('selected');
 
-    $('#main-image a').attr('large', selectedThumb.attr('href'));
-    $('#large_image_overlay .large_image img').attr('src', selectedThumb.attr('href'));
+    $('#main-image a').attr('large', selectedThumb.attr('large'));
+    $('#large_image_overlay .large_image img').attr('src', selectedThumb.attr('large'));
     e.preventDefault();
   });
 
@@ -40,8 +40,10 @@ var add_image_handlers = function() {
         });
       });
 
+
       $('#large_image_overlay .thumbs_gallery a').click(function(e) {
         var selectedThumb = $(this);
+        if (large_image.attr('src') != selectedThumb.attr('href'))
         container.animate({ 'opacity': 0.01 }, 200, function() {
           large_image.attr('src', selectedThumb.attr('href'));
         });
@@ -83,7 +85,7 @@ function getThumbnails() {
   return $('ul.thumbnails li').map(function() {
     return {
       'thumb': $(this).find('img').attr('src'),
-      'large': $(this).find('a').attr('href')
+      'large': $(this).find('a').attr('large')
     };
   });
 }
